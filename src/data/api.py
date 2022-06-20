8973b932c889b5abe7c484cd5b85a1e2179f9687
jordipg37 <jordipuig37@gmail.com>
2022-06-20 20:04:56 +0200

    pep8ed api

--- src/data/api.py
+++ src/data/api.py
@@ -3,8 +3,12 @@ import requests
 from urllib.parse import quote
 import json
 
-def coingecko_symbol_history(symbol: str, start_date: datetime = datetime(2020,12,31), end_date: datetime = datetime.now()) -> str:
-    """Coin history, in USD. 
+
+def coingecko_symbol_history(
+        symbol: str,
+        start_date: datetime = datetime(2020, 12, 31),
+        end_date: datetime = datetime.now()) -> str:
+    """Coin history, in USD.
     Symbol example: 'bitcoin' or 'ethereum'
     Default timestamps: Start (December 31, 2020), End (now)
     """
@@ -13,19 +17,29 @@ def coingecko_symbol_history(symbol: str, start_date: datetime = datetime(2020,1
     end = int(end_date.timestamp())
     return f'https://api.coingecko.com/api/v3/coins/{s}/market_chart/range?vs_currency=usd&from={start}&to={end}'
 
-def deribit_symbol_history(symbol: str, resolution: str = '60', start_date: datetime = datetime(2020,12,31), end_date: datetime = datetime.now()) -> str:
-    """Contract symbol price history. 
+
+def deribit_symbol_history(
+        symbol: str,
+        resolution: str = '60',
+        start_date: datetime = datetime(2020, 12, 31),
+        end_date: datetime = datetime.now()) -> str:
+    """Contract symbol price history.
     Symbol example: 'BTC-1JUL22-12000-C'
     Resolution: int (minutes) or 1D
     Default timestamps: Start (December 31, 2020), End (now)
     """
     s = quote(symbol)
     r = quote(resolution)
-    start = int(start_date.timestamp()*1000)
-    end = int(end_date.timestamp()*1000)
+    start = int(start_date.timestamp() * 1000)
+    end = int(end_date.timestamp() * 1000)
     return f'https://www.deribit.com/api/v2/public/get_tradingview_chart_data?start_timestamp={start}&end_timestamp={end}&instrument_name={s}&resolution={r}'
 
-def deribit_vol_index(symbol: str, resolution: str = '3600', start_date: datetime = datetime(2020,12,31), end_date: datetime = datetime.now()) -> str:
+
+def deribit_vol_index(
+        symbol: str,
+        resolution: str = '3600',
+        start_date: datetime = datetime(2020, 12, 31),
+        end_date: datetime = datetime.now()) -> str:
     """Volatility history.
     Symbol example: 'BTC'
     Resolution: int (seconds) or 1D
@@ -33,19 +47,24 @@ def deribit_vol_index(symbol: str, resolution: str = '3600', start_date: datetim
     """
     r = quote(str(resolution))
     s = quote(symbol)
-    start = int(start_date.timestamp()*1000)
-    end = int(end_date.timestamp()*1000)
+    start = int(start_date.timestamp() * 1000)
+    end = int(end_date.timestamp() * 1000)
     return f'https://www.deribit.com/api/v2/public/get_volatility_index_data?currency={s}&start_timestamp={start}&end_timestamp={end}&resolution={r}'
+
 
 # Not used yet
 def deribit_ticker(symbol: str) -> str:
-    """Ticker metadata, greeks and IV. 
+    """Ticker metadata, greeks and IV.
     Symbol example: 'BTC-1JUL22-12000-C'
     """
     s = quote(symbol)
     return f'https://www.deribit.com/api/v2/public/ticker?instrument_name={s}'
 
-def deribit_all_instruments(symbol: str, kind: str = 'option', expired: bool = False) -> str:
+
+def deribit_all_instruments(
+        symbol: str,
+        kind: str = 'option',
+        expired: bool = False) -> str:
     """Get all instruments.
     Symbol example: 'BTC'
     Kind: 'option' or 'future'
@@ -57,8 +76,7 @@ def deribit_all_instruments(symbol: str, kind: str = 'option', expired: bool = F
     return f'https://www.deribit.com/api/v2/public/get_instruments?currency={s}&expired={e}&kind={k}'
 
 
-
-def get_deribit_symbols(coin):
+def get_deribit_symbols(coin: str) -> list:
     try:
         api_url = deribit_all_instruments(coin)
         raw = requests.get(api_url)
@@ -67,46 +85,49 @@ def get_deribit_symbols(coin):
 
         print('Get Symbols -- Query successful')
         return symbols
-    except:
+    except BaseException:
         print('Get Symbols -- Query fail')
         if raw:
             print(raw.json())
         return []
 
-def get_deribit_symbol(symbol):
+
+def get_deribit_symbol(symbol: str) -> dict:
     try:
         api_url = deribit_symbol_history(symbol)
         raw = requests.get(api_url)
         data = raw.json()['result']
-        
+
         print(symbol, '-- Price history -- Query successful')
         return data
-    except:
+    except BaseException:
         print(symbol, '-- Price history -- Query fail')
         if raw:
             print(raw.json())
         return {}
 
-def get_deribit_vol(symbol, end_date = datetime.now()):
+
+def get_deribit_vol(symbol: str, end_date=datetime.now()) -> dict:
     try:
         api_url = deribit_vol_index(symbol, end_date=end_date)
         raw = requests.get(api_url)
         data = raw.json()['result']
-        
+
         print(symbol, end_date, '-- Volatility history -- Query successful')
 
         if data['continuation']:
-            new_end_date = datetime.fromtimestamp(data['continuation']/1000)
+            new_end_date = datetime.fromtimestamp(data['continuation'] / 1000)
             data['data'] += get_deribit_vol(symbol, new_end_date)['data']
 
         return data
-    except:
+    except BaseException:
         print(symbol, end_date, '-- Volatility history -- Query fail')
         if raw:
             print(raw.json())
         return {}
-    
-def get_coingecko_symbol(symbol):
+
+
+def get_coingecko_symbol(symbol: str) -> dict:
     try:
         api_url = coingecko_symbol_history(symbol)
         raw = requests.get(api_url)
@@ -114,32 +135,34 @@ def get_coingecko_symbol(symbol):
 
         print(symbol, '-- Price history -- Query successful')
         return data
-    except:
+    except BaseException:
         print(symbol, '-- Price history -- Query fail')
         if raw:
             print(raw.json())
         return {}
 
+
 def save_deribit_symbols(coin):
     symbols = get_deribit_symbols(coin)
-    symbols = symbols[:10] # for test
+    symbols = symbols[:10]  # for test
 
     data = dict(zip(symbols, map(get_deribit_symbol, symbols)))
 
     with open(f'./data/raw/deribit_symbols_{coin}.json', 'w') as raw:
         json.dump(data, raw)
 
+
 def save_deribit_vol(symbol):
     data = get_deribit_vol(symbol)
-
     with open(f'./data/raw/deribit_vol_{symbol}.json', 'w') as raw:
         json.dump(data, raw)
 
+
 def save_coingecko_symbol(symbol):
     data = get_coingecko_symbol(symbol)
-    
     with open(f'./data/raw/coingecko_symbol_{symbol}.json', 'w') as raw:
         json.dump(data, raw)
+
 
 def main():
     save_deribit_symbols('BTC')
@@ -151,4 +174,6 @@ def main():
     save_coingecko_symbol('bitcoin')
     save_coingecko_symbol('ethereum')
 
-main()
+
+if __name__ == "__main__":
+    main()
