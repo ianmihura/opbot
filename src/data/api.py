8f682a8457c4a4311e052d335fb1bad10c21430b
Ian Mihura <mihura.ian@gmail.com>
2022-06-20 19:48:47 +0200

    get api symbols

--- src/data/api.py
+++ src/data/api.py
@@ -4,6 +4,9 @@ load_dotenv()
 from datetime import datetime
 import requests
 from urllib.parse import quote
+import json
+
+import pdb
 
 def coingecko_symbol_history(symbol: str, start_date: datetime = datetime(2020,12,31), end_date: datetime = datetime.now()) -> str:
     """Coin history, in USD. 
@@ -11,12 +14,12 @@ def coingecko_symbol_history(symbol: str, start_date: datetime = datetime(2020,1
     Default timestamps: Start (December 31, 2020), End (now)
     """
     s = quote(symbol)
-    start = int(start_date.timestamp()*1000)
-    end = int(end_date.timestamp()*1000)
+    start = int(start_date.timestamp())
+    end = int(end_date.timestamp())
     return f'https://api.coingecko.com/api/v3/coins/{s}/market_chart/range?vs_currency=usd&from={start}&to={end}'
 
 def deribit_symbol_history(symbol: str, resolution: str = '60', start_date: datetime = datetime(2020,12,31), end_date: datetime = datetime.now()) -> str:
-    """Symbol price history. 
+    """Contract symbol price history. 
     Symbol example: 'BTC-1JUL22-12000-C'
     Resolution: int (minutes) or 1D
     Default timestamps: Start (December 31, 2020), End (now)
@@ -39,6 +42,7 @@ def deribit_vol_index(symbol: str, resolution: str = '3600', start_date: datetim
     end = int(end_date.timestamp()*1000)
     return f'https://www.deribit.com/api/v2/public/get_volatility_index_data?currency={s}&start_timestamp={start}&end_timestamp={end}&resolution={r}'
 
+# Not used yet
 def deribit_ticker(symbol: str) -> str:
     """Ticker metadata, greeks and IV. 
     Symbol example: 'BTC-1JUL22-12000-C'
@@ -60,34 +64,119 @@ def deribit_all_instruments(symbol: str, kind: str = 'option', expired: bool = F
 
 
 
-# Coin Gecko API. origin: 1643932800000
-def correct_crypto_time(x):
-    # TODO: what timestamp do we need?
-    return int(datetime.timestamp(datetime.fromtimestamp(x[0]/1000).replace(hour=0)))
+# # Coin Gecko API. origin: 1643932800000
+# def correct_crypto_time(x):
+#     # TODO: what timestamp do we need?
+#     return int(datetime.timestamp(datetime.fromtimestamp(x[0]/1000).replace(hour=0)))
 
-# Formatting raw input
-def format_coingecko(jraw):
-    data = jraw['prices']
-    timestamps = [*map(correct_crypto_time, data)]
-    prices = [*map(lambda x: x[1], data)]
-    z_data = [*zip(timestamps, prices)]
-    z_data.reverse()
-    return z_data
+# # Formatting raw input
+# def format_coingecko(jraw):
+#     data = jraw['prices']
+#     timestamps = [*map(correct_crypto_time, data)]
+#     prices = [*map(lambda x: x[1], data)]
+#     z_data = [*zip(timestamps, prices)]
+#     z_data.reverse()
+#     return z_data
 
-def main() -> None:
-    # welcome
+def get_deribit_symbols(coin):
+    try:
+        api_url = deribit_all_instruments(coin)
+        raw = requests.get(api_url)
+        data = raw.json()['result']
+        symbols = [s['instrument_name'] for s in data if s['kind'] == 'option']
 
-    return
+        print('Get Symbols -- Query successful')
+        return symbols
+    except:
+        print('Get Symbols -- Query fail')
+        if raw:
+            print(raw.json())
+        return []
 
-# Main save asset: chooses what to do
-# def save_asset(asset, asset_api_id):
-#     try:
-#         api_url = get_api_coingecko(asset_api_id)
-#         raw = requests.get(api_url) # this is the REST call
-#         data = format_coingecko(raw.json())
+def get_deribit_symbol(symbol):
+    try:
+        api_url = deribit_symbol_history(symbol)
+        raw = requests.get(api_url)
+        data = raw.json()['result']
         
-#         # TODO: save data
-#         print(asset, '-- Price -- Query successful')
-#     except:
-#         print(asset, '-- Price -- Query fail')
-#         print(raw.json())
+        print(symbol, '-- Price history -- Query successful')
+        return data
+    except:
+        print(symbol, '-- Price history -- Query fail')
+        if raw:
+            print(raw.json())
+        return {}
+
+def get_deribit_vol(symbol, end_date = datetime.now()):
+    try:
+        api_url = deribit_vol_index(symbol, end_date=end_date)
+        raw = requests.get(api_url)
+        data = raw.json()['result']
+        
+        print(symbol, end_date, '-- Volatility history -- Query successful')
+
+        if data['continuation']:
+            new_end_date = datetime.fromtimestamp(data['continuation']/1000)
+            data['data'] += get_deribit_vol(symbol, new_end_date)['data']
+
+        return data
+    except:
+        print(symbol, end_date, '-- Volatility history -- Query fail')
+        if raw:
+            print(raw.json())
+        return {}
+    
+def get_coingecko_symbol(symbol):
+    try:
+        api_url = coingecko_symbol_history(symbol)
+        raw = requests.get(api_url)
+        data = raw.json()
+
+        print(symbol, '-- Price history -- Query successful')
+        return data
+    except:
+        print(symbol, '-- Price history -- Query fail')
+        if raw:
+            print(raw.json())
+        return {}
+
+def save_deribit_symbols(coin):
+    symbols = get_deribit_symbols(coin)
+    symbols = symbols[:10] # for test
+
+    data = dict(zip(symbols, map(get_deribit_symbol, symbols)))
+
+    with open(f'./data/raw/deribit_symbols_{coin}.json', 'w') as raw:
+        json.dump(data, raw)
+
+def save_deribit_vol(symbol):
+    data = get_deribit_vol(symbol)
+
+    with open(f'./data/raw/deribit_vol_{symbol}.json', 'w') as raw:
+        json.dump(data, raw)
+
+def save_coingecko_symbol(symbol):
+    data = get_coingecko_symbol(symbol)
+    
+    with open(f'./data/raw/coingecko_symbol_{symbol}.json', 'w') as raw:
+        json.dump(data, raw)
+
+def main():
+    save_deribit_symbols('BTC')
+    save_deribit_symbols('ETH')
+
+    save_deribit_vol('BTC')
+    save_deribit_vol('ETH')
+
+    save_coingecko_symbol('bitcoin')
+    save_coingecko_symbol('ethereum')
+
+    # f = open('./data/raw/deribit_vol_BTC.json')
+    # symbols = json.load(f)
+    # timestamps = list(map(lambda x: datetime.fromtimestamp(x[0]/1000), symbols))
+    # timestamps = [list(map(lambda x: datetime.fromtimestamp(x/1000), symbols[symbol]['ticks'])) for symbol in symbols]
+    # print([t[0] for t in timestamps if len(t)])
+    # print(timestamps)
+
+
+main()
