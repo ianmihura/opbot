2e11ed9540ddc933817bb5158eca7803afafa1de
jordipg37 <jordipuig37@gmail.com>
2022-07-07 14:24:10 +0200

    updates on make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
-import click
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
-from types import SimpleNamespace
+from dotenv import find_dotenv, load_dotenv, dotenv_values
+
 import sqlite3
 from datetime import datetime
+import argparse
 
 from preprocess import main as preprocess_main
 from api import main as api_main
@@ -16,10 +15,7 @@ from insert_dataset import insert_connection
 import pdb
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main(args):
     """ Runs data processing scripts to turn raw data from (../raw) into
     cleaned data ready to be analyzed (saved in ../processed).
     """
@@ -32,7 +28,7 @@ def main(input_filepath, output_filepath):
     preprocess_main()
 
     # read and connect to the output filepath (destination DW)
-    con = sqlite3.connect(os.path.join(output_filepath, 'datawarehouse.db'))
+    con = sqlite3.connect(os.path.join(args.output_filepath, DATA_WAREHOUSE_FILE))
     
     logger.info('inserting data into the destination database')
     insert_connection(con)
@@ -42,11 +38,10 @@ if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    parser = argparse.ArgumentParser(description='Run data processing scripts to turn raw data from (../raw) into clean data ready to be analyzed (saved in ../processed).')
+    parser.add_argument('-i', '--input_filepath', help='input filepath', required=True)
+    parser.add_argument('-o', '--output_filepath', help='output filepath', required=True)
+    args = parser.parse_args()
+    # add arguments from .env to the namespace 
+    args = argparse.Namespace(**vars(args), **dotenv_values(find_dotenv()))
+    main(args)
